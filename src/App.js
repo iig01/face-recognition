0205bc2becf4040701e5f9266b3c611710308acc
iig01 <iisgg20@gmail.com>
2022-09-05 10:20:15 +0500

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navigation from "./Components/Navigation";
+import Logo from "./Components/Logo";
+import ImageLinkForm from "./Components/ImageLinkForm";
+import Rank from "./Components/Rank";
+import Particle from "./Components/Particle/Particle";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particle />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition/> */}
     </div>
   );
 }
---
+++ src/Components/Rank/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+const Rank = () => {
+  return (
+    <div>
+      <div className="white f3">{"Ismail, your current rank is ..."}</div>
+      <div className="white f1">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
